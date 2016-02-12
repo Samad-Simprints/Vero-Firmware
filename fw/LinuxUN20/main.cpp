f79050512cef0d06c24c1b40a245d9c359af2a9e
Nick Terry <nick.terry@fentechnology.co.uk>
2016-02-12 17:48:01 +0000

    Set template format to ISO
    git-svn-id: http://svn:8000/svn/INDEX/trunk@65 71e9ff3b-2aa0-ee42-b4dd-db5a0b4f3664

--- fw/LinuxUN20/main.cpp
+++ fw/LinuxUN20/main.cpp
@@ -57,7 +57,7 @@ extern "C" {
 
 /* baudrate settings are defined in <asm/termbits.h>, which is
 included by <termios.h> */
-#define BAUDRATE B9600            
+#define BAUDRATE B9600
 /* change this definition for the correct port */
 //#define MODEMDEVICE "/dev/ttyGS0"
 #define MODEMDEVICE "/dev/ttyS1"
@@ -88,17 +88,17 @@ static int serial_config(char *port, struct termios *oldtio)
 
   printf("Configuring: %s\n", port);
 
-  /* 
+  /*
   Open modem device for reading and writing and not as controlling tty
   because we don't want to get killed if linenoise sends CTRL-C.
   */
-  fd = open(port, O_RDWR | O_NOCTTY ); 
+  fd = open(port, O_RDWR | O_NOCTTY );
   if (fd <0) {perror(port); exit(-1); }
 
   tcgetattr(fd,oldtio); /* save current serial port settings */
   bzero(&newtio, sizeof(newtio)); /* clear struct for new port settings */
 
-  /* 
+  /*
   BAUDRATE: Set bps rate. You could also use cfsetispeed and cfsetospeed.
   CRTSCTS : output hardware flow control (only used if the cable has
             all necessary lines. See sect. 7 of Serial-HOWTO)
@@ -107,7 +107,7 @@ static int serial_config(char *port, struct termios *oldtio)
   CREAD   : enable receiving characters
   */
   newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
- 
+
   /*
   IGNPAR  : ignore bytes with parity errors
   ICRNL   : map CR to NL (otherwise a CR input on the other computer
@@ -115,24 +115,24 @@ static int serial_config(char *port, struct termios *oldtio)
   otherwise make device raw (no other input processing)
   */
   newtio.c_iflag = IGNPAR;
- 
+
   /*
   Raw output.
   */
   newtio.c_oflag = 0;
- 
+
   /*
   ICANON  : enable canonical input
   disable all echo functionality, and don't send signals to calling program
   */
  //newtio.c_lflag = ICANON;
- 
-  /* 
-  initialize all control characters 
+
+  /*
+  initialize all control characters
   default values can be found in /usr/include/termios.h, and are given
   in the comments, but we don't need them here
   */
-  newtio.c_cc[VINTR]    = 0;     /* Ctrl-c */ 
+  newtio.c_cc[VINTR]    = 0;     /* Ctrl-c */
   newtio.c_cc[VQUIT]    = 0;     /* Ctrl-\ */
   newtio.c_cc[VERASE]   = 0;     /* del */
   newtio.c_cc[VKILL]    = 0;     /* @ */
@@ -140,7 +140,7 @@ static int serial_config(char *port, struct termios *oldtio)
   newtio.c_cc[VTIME]    = 0;     /* inter-character timer unused */
   newtio.c_cc[VMIN]     = 1;     /* blocking read until 1 character arrives */
   newtio.c_cc[VSWTC]    = 0;     /* '\0' */
-  newtio.c_cc[VSTART]   = 0;     /* Ctrl-q */ 
+  newtio.c_cc[VSTART]   = 0;     /* Ctrl-q */
   newtio.c_cc[VSTOP]    = 0;     /* Ctrl-s */
   newtio.c_cc[VSUSP]    = 0;     /* Ctrl-z */
   newtio.c_cc[VEOL]     = 0;     /* '\0' */
@@ -150,7 +150,7 @@ static int serial_config(char *port, struct termios *oldtio)
   newtio.c_cc[VLNEXT]   = 0;     /* Ctrl-v */
   newtio.c_cc[VEOL2]    = 0;     /* '\0' */
 
-  /* 
+  /*
   now clean the modem line and activate the settings for the port
   */
   tcflush(fd, TCIFLUSH);
@@ -206,7 +206,7 @@ static int un20_sdk_config()
   printf("deviceInfo.ImageDPI   : %ld\n", deviceInfo.ImageDPI);
   printf("deviceInfo.FWVersion  : %04X\n", (unsigned int) deviceInfo.FWVersion);
 
-  ImageBuffer = (BYTE*) malloc(deviceInfo.ImageHeight * deviceInfo.ImageWidth); 
+  ImageBuffer = (BYTE*) malloc(deviceInfo.ImageHeight * deviceInfo.ImageWidth);
   if ( !ImageBuffer )
   {
      printf("ERROR - Unable to alocate image buffer.\n\n");
@@ -258,7 +258,7 @@ static void receiver(int fd)
 }
 
 static DWORD templateSize;
-static BYTE *minutiaeBuffer1 = NULL; 
+static BYTE *minutiaeBuffer1 = NULL;
 
 // Called when a protocol message has been received from the UN20 or phone.
 static void vMessageProcess( MsgInternalPacket *psMsg )
@@ -311,10 +311,10 @@ static void vMessageProcess( MsgInternalPacket *psMsg )
 
     // generate template for image
     case MSG_GENERATE_TEMPLATE:
-      err = sgfplib->SetTemplateFormat(TEMPLATE_FORMAT_ANSI378);
+      err = sgfplib->SetTemplateFormat(TEMPLATE_FORMAT_ISO19794);
       err |= sgfplib->GetMaxTemplateSize(&templateSizeMax);
 
-      minutiaeBuffer1 = (BYTE*) realloc(minutiaeBuffer1, templateSizeMax); 
+      minutiaeBuffer1 = (BYTE*) realloc(minutiaeBuffer1, templateSizeMax);
       fingerInfo.FingerNumber = SG_FINGPOS_UK;
       fingerInfo.ViewNumber = 1;
       fingerInfo.ImpressionType = SG_IMPTYPE_LP;
