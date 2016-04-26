960643e89f54e28d3856049e39c2a83cb8813eec
Nick Terry <nick.terry@fentechnology.co.uk>
2016-04-26 12:55:45 +0000

    Changing BT friendly name from Simprints-XXXXXX to SPXXXXXX as requested by Alex.
    git-svn-id: http://svn:8000/svn/INDEX/trunk@90 71e9ff3b-2aa0-ee42-b4dd-db5a0b4f3664

--- fw/dotstack/samples/common/app/speed-test/btapp_speed-test.c
+++ fw/dotstack/samples/common/app/speed-test/btapp_speed-test.c
@@ -217,7 +217,7 @@ static char device_name[32] = { "Undefined" };
 
 const char* bt_oem_get_device_name(void)
 {
-  snprintf(device_name, sizeof(device_name), "Simprints-%06X", (mModuleAddress.bd_addr_m & 0x00FFFFFF));
+  snprintf(device_name, sizeof(device_name), "SP%06X", (mModuleAddress.bd_addr_m & 0x00FFFFFF));
   return device_name;
 }
 
