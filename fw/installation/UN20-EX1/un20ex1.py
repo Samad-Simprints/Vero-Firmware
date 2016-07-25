2ce28cc687f87b9e8cf6cd4a52efe4db902ec866
limaechocharlie <james@simprints.com>
2016-07-25 19:51:20 +0000

    UN20 install: add human friendly error messages and subprocess timeout

--- fw/installation/UN20-EX1/un20ex1.py
+++ fw/installation/UN20-EX1/un20ex1.py
@@ -6,26 +6,20 @@ import platform
 import sys
 
 sys_name = platform.system()
+unit_name = 'UN20-EX1'
+process_timeout = 10
 
-if sys_name == 'Windows':
-	processes = (
-			# start ftp server on un20ex1 process 
-			[settings.PLINK,'-ssh','root@%s'%settings.HOST,'-i','%s'%settings.PPK,'-m', 'start-ftp-service.sh','-N'],
-			# transfer files using WinSCP to un20ex1
-			[settings.WINSCP, '/script=winscp-ftp-commands.txt'],
-			# call install script on un20ex1
-			[settings.PLINK,'-ssh','root@%s'%settings.HOST,'-i','%s'%settings.PPK,'-m', 'call-install-index.sh','-N'],
-		    )
-elif sys_name == 'Linux':
+if sys_name == 'Linux':
 	processes = (
 			# set ip address (assumes that the UN20 will appear as usb0)
-			['sudo','ifconfig','usb0','10.0.1.2','netmask','255.255.0.0'],
+			(['sudo','ifconfig','usb0','10.0.1.2','netmask','255.255.0.0'], '--> ERROR: Unable to set IP address of %s'%unit_name),
 			# start ftp server on un20ex1 process 
-			['ssh','root@%s'%settings.HOST,'inetd /etc/inetd.conf; exit'],
+			(['ssh','root@%s'%settings.HOST,'inetd /etc/inetd.conf; exit'], '--> ERROR: Can not start ftp server on %s'%unit_name),
 			# transfer files using ftp to un20ex1
-			['./push-files.sh', settings.BUILD_PATH],
+			(['./push-files.sh', settings.BUILD_PATH], '--> ERROR: file transfer to %s has failed'%unit_name),
 			# call install script on un20ex1
-			['ssh','root@%s'%settings.HOST,'cd /data; chmod +x install-index-un20; sync; source install-index-un20; halt; exit'],
+			(['ssh','root@%s'%settings.HOST,'cd /data; chmod +x install-index-un20; sync; source install-index-un20; halt; exit'], 
+				'--> ERROR: calling install script on %s has failed'%unit_name)
 		    )
 else:
     raise Exception
@@ -60,17 +54,22 @@ def installUN20():
     print( '--> serialNumber: %s' % serialNumber )
 
     ## run each process
-    for process in processes:
-        if sys.version_info >= (3,5):
-            # run added in Python 3.5
-            progRet = subprocess.run( process, stderr = subprocess.DEVNULL )
-            returncode = progRet.returncode	
-        else:
-            returncode = subprocess.call( process )
-        if returncode != 0:
-            status = 'Failed'
-            print('--> WARNING: Failed to program')
-            break
+    try:
+        for process in processes:
+            if sys.version_info >= (3,5):
+                # run added in Python 3.5
+                progRet = subprocess.run( process[0], stderr = subprocess.DEVNULL, timeout = process_timeout )
+                returncode = progRet.returncode	
+            else:
+                returncode = subprocess.call( process[0], timeout = process_timeout)
+            if returncode != 0:
+                status = 'Failed'
+                print(process[1])
+                break
+    except subprocess.TimeoutExpired:
+        print( '--> ERROR: Subprocess timeout' )
+        status = 'Failed'
+    
 
     # post status
     try:
@@ -84,14 +83,14 @@ def installUN20():
                               params = {'apiKey' : apiKeyVal, 'WorkOrder' : workOrder, 'Serial': serialNumber})
     if response.ok:
         data = response.json()
-        print( '--> Does exist (Id = %d), PUT'%data['Id'])
+        print( '--> PUT id = %s'%data['Id'])
         #print( data )
         data['Status'] = status
         response = requests.put( '%s/%s/%d'%(settings.MANU_URL, 'PutInterfacePCB', data['Id']), 
                                   params = {'apiKey' : apiKeyVal}, data = data)
     elif response.status_code == 404:
         # Can't find, so post
-        print( '--> Does not exist, POST')
+        print( '--> POST')
         data = {'WorkOrder': workOrder, 'SerialNumber':serialNumber, 'Status' : status}
         #print( data )
         response = requests.post( '%s/%s'%(settings.MANU_URL, 'PostInterfacePCB'), 
@@ -100,6 +99,10 @@ def installUN20():
         
     # raise anything untoward in response
     response.raise_for_status()
+    if status == 'Passed':
+        print('\n\n\n\t\tPASSED\n\n\n')
+    else:
+        print('\n\n\n\t\tFAILED\n\n\n')
     return
 
 if __name__ == '__main__':
