f9e7dd9afc116b3c509d94311886fff33e327897
LimaEchoCharlie <james@simprints.com>
2016-09-15 17:59:09 +0100

    Start to compile c files using gcc for unit testing

---
+++ fw/tests/app/crc_test.cpp
@@ -0,0 +1,17 @@
+
+#include "CppUTest/TestHarness.h"
+
+TEST_GROUP(crc)
+{
+    void setup()
+    {
+    }
+    void teardown()
+    {
+    }
+}
+
+TEST( crc, crcGen_One)
+{
+    FAIL("Start here");
+}
